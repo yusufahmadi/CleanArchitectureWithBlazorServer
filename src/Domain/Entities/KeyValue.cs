560e9981950caf2a2f17a68296234a721f5e901d
yahmadi <yusuf.ahmadi@profescipta.co.id>
2024-07-22 01:14:38 +0700

    add pick list enum

--- src/Domain/Entities/KeyValue.cs
+++ src/Domain/Entities/KeyValue.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.ComponentModel;
@@ -18,5 +18,8 @@ public enum Picklist
 {
     [Description("Status")] Status,
     [Description("Unit")] Unit,
-    [Description("Brand")] Brand
+    [Description("Brand")] Brand,
+    [Description("JenisKendaraan")] JenisKendaraan,
+    [Description("Merk")] Merk,
+
 }
